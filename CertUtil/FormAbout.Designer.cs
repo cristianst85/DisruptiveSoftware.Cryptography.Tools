dc98c611df81e9597990f75b090248cd267da083
Cristian Stoica <cristianstoica85@gmail.com>
2023-09-30 01:28:24 +0300

    About dialogs internal improvements.

--- CertUtil/FormAbout.Designer.cs
+++ CertUtil/FormAbout.Designer.cs
@@ -30,7 +30,7 @@
             this.richTextBoxCopyright = new System.Windows.Forms.RichTextBox();
             this.linkLabelSource = new System.Windows.Forms.LinkLabel();
             this.labelVersion = new System.Windows.Forms.Label();
-            this.labelPluginName = new System.Windows.Forms.Label();
+            this.labelName = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.linkLabelContact = new System.Windows.Forms.LinkLabel();
             this.label3 = new System.Windows.Forms.Label();
@@ -41,7 +41,7 @@
             this.label4.Location = new System.Drawing.Point(21, 73);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(256, 15);
-            this.label4.TabIndex = 13;
+            this.label4.TabIndex = 4;
             this.label4.Text = "Released under GNU GPLv2 or later.";
             this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
@@ -51,8 +51,9 @@
             this.label6.Location = new System.Drawing.Point(21, 147);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(94, 13);
-            this.label6.TabIndex = 17;
+            this.label6.TabIndex = 8;
             this.label6.Text = "Portions copyright:";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // richTextBoxCopyright
             // 
@@ -62,7 +63,7 @@
             this.richTextBoxCopyright.ReadOnly = true;
             this.richTextBoxCopyright.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedVertical;
             this.richTextBoxCopyright.Size = new System.Drawing.Size(255, 58);
-            this.richTextBoxCopyright.TabIndex = 18;
+            this.richTextBoxCopyright.TabIndex = 9;
             this.richTextBoxCopyright.Text = resources.GetString("richTextBoxCopyright.Text");
             // 
             // linkLabelSource
@@ -71,9 +72,10 @@
             this.linkLabelSource.Location = new System.Drawing.Point(21, 88);
             this.linkLabelSource.Name = "linkLabelSource";
             this.linkLabelSource.Size = new System.Drawing.Size(113, 13);
-            this.linkLabelSource.TabIndex = 14;
+            this.linkLabelSource.TabIndex = 5;
             this.linkLabelSource.TabStop = true;
             this.linkLabelSource.Text = "GitHub project source.";
+            this.linkLabelSource.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // labelVersion
             // 
@@ -81,20 +83,19 @@
             this.labelVersion.Location = new System.Drawing.Point(21, 35);
             this.labelVersion.Name = "labelVersion";
             this.labelVersion.Size = new System.Drawing.Size(259, 15);
-            this.labelVersion.TabIndex = 11;
+            this.labelVersion.TabIndex = 2;
             this.labelVersion.Text = "Version {version}";
             this.labelVersion.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
-            // labelPluginName
+            // labelName
             // 
-            this.labelPluginName.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelPluginName.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.labelPluginName.Location = new System.Drawing.Point(21, 20);
-            this.labelPluginName.Name = "labelPluginName";
-            this.labelPluginName.Size = new System.Drawing.Size(259, 15);
-            this.labelPluginName.TabIndex = 10;
-            this.labelPluginName.Text = "CertUtil";
-            this.labelPluginName.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.labelName.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelName.Location = new System.Drawing.Point(21, 20);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(259, 15);
+            this.labelName.TabIndex = 1;
+            this.labelName.Text = "CertUtil";
+            this.labelName.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // label5
             // 
@@ -102,7 +103,7 @@
             this.label5.Location = new System.Drawing.Point(21, 108);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(255, 15);
-            this.label5.TabIndex = 16;
+            this.label5.TabIndex = 6;
             this.label5.Text = "Contact:";
             this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
@@ -112,16 +113,17 @@
             this.linkLabelContact.Location = new System.Drawing.Point(21, 123);
             this.linkLabelContact.Name = "linkLabelContact";
             this.linkLabelContact.Size = new System.Drawing.Size(138, 13);
-            this.linkLabelContact.TabIndex = 15;
+            this.linkLabelContact.TabIndex = 7;
             this.linkLabelContact.TabStop = true;
             this.linkLabelContact.Text = "cristianstoica85@gmail.com";
+            this.linkLabelContact.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // label3
             // 
             this.label3.Location = new System.Drawing.Point(21, 58);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(256, 15);
-            this.label3.TabIndex = 12;
+            this.label3.TabIndex = 3;
             this.label3.Text = "Copyright (c) 2019-2023 Cristian Stoica.\r\n";
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
@@ -135,7 +137,7 @@
             this.Controls.Add(this.richTextBoxCopyright);
             this.Controls.Add(this.linkLabelSource);
             this.Controls.Add(this.labelVersion);
-            this.Controls.Add(this.labelPluginName);
+            this.Controls.Add(this.labelName);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.linkLabelContact);
             this.Controls.Add(this.label3);
@@ -160,7 +162,7 @@
         private System.Windows.Forms.RichTextBox richTextBoxCopyright;
         private System.Windows.Forms.LinkLabel linkLabelSource;
         private System.Windows.Forms.Label labelVersion;
-        private System.Windows.Forms.Label labelPluginName;
+        private System.Windows.Forms.Label labelName;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.LinkLabel linkLabelContact;
         private System.Windows.Forms.Label label3;
